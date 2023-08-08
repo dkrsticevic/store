095d50fa9e69abc63b2e4920f61cee28598e2650
Daniel Krsticevic <danielkrsticevic@gmail.com>
2023-08-08 13:41:13 +0800

    Added back button to store page on sign up and login pages

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -9,9 +9,19 @@ export default function Signup() {
 
     return (
         <Container style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+
             <Card style={{width: "100%", maxWidth: "500px", marginTop: "200px"}}>
                 <Card.Body>
-                    <Card.Title>Log In</Card.Title>
+                    <Card.Title style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                        <Link to="/">
+                        <Button variant='light' style={{borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", height: "40px", aspectRatio: "1/1"}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-caret-left" viewBox="0 0 16 16">
+                            <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+                            </svg>
+                        </Button> 
+                        </Link>
+                        <h3>Log In</h3>
+                        <div style={{width: "40px"}}></div>
+                     </Card.Title>
                     <Form> 
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
