f34105a72a6a8e9742d0000914f8671cb443eeb4
Daniel Krsticevic <danielkrsticevic@gmail.com>
2023-08-17 12:40:14 +0800

    Changed store items ui

--- client/src/components/StoreItem.tsx
+++ client/src/components/StoreItem.tsx
@@ -2,24 +2,22 @@ import { Button, Card } from "react-bootstrap"
 import { useCart } from "../context/CartContext"
 
 type StoreItemProps = {
-    name : string
-    id: number
-    price: number
+    item : {id: number, name: string, price: number, image: string}
 }
 
-function StoreItem({name, id, price}: StoreItemProps) {
+function StoreItem({item}: StoreItemProps) {
   const { addToCart, getQuantity, removeFromCart } = useCart()
-  const count = getQuantity(id)
+  const count = getQuantity(item.id)
 
 
   return (
-    <Card key={id} style={{width: "300px", height: "300px"}}>
+    <Card key={item.id} style={{width: "350px"}}>
         <Card.Body style={{height: "100%", width: "100%"}}>
-        <Card.Title>{name}</Card.Title>
-        <span>${price} AUD</span>
-        <div>{count !== 0 && `${count} in Cart`}</div>
-        <Button style={{width: "88%", position: "absolute", bottom: 62,}} onClick={() => addToCart(id, price)}>Add To Cart</Button>
-        <Button disabled={count == 0 && true} style={{width: "88%" , position: "absolute", bottom: 12}} onClick={() => removeFromCart(id)}>Remove from Cart</Button> 
+        <Card.Title style={{display: "flex", justifyContent: "space-between"}}><h3>{item.name}</h3><h4>${item.price} AUD</h4></Card.Title>
+        <div style={{position: "absolute", top: "45px", right: "16px"}}>{count !== 0 && `${count} in Cart`}</div>
+        <Card.Img src={item.image} style={{height: "200px", width: "100%", objectFit: "contain", border: "1px solid black", marginTop: "5px", marginBottom: "5px"}}></Card.Img>
+        <Button className="w-100" style={{marginBottom: "5px"}} onClick={() => addToCart(item.id, item.price)}>Add To Cart</Button>
+        <Button className="w-100" disabled={count == 0 && true} onClick={() => removeFromCart(item.id)}>Remove from Cart</Button> 
         </Card.Body>
     </Card>
   )
--- client/src/pages/Store.tsx
+++ client/src/pages/Store.tsx
@@ -3,16 +3,14 @@ import items from "../data/items.json"
 import StoreItem from "../components/StoreItem";
 
 
-//const items : {id: number, name: string, price: number}[] = [{id: 1, name: "item1", price: 100}, {id: 2, name: "item2", price: 120}, {id: 3, name: "item3", price: 130}]
-
 function Store() {
   return (
     <>
     <Container style={{display: "flex", justifyContent: "center"}}>
-        <Row md={2} s={1} lg={3} style={{display: "flex", justifyContent: "left", marginTop: "30px", width: "100%", maxWidth: "900px"}}>
+        <Row md={2} s={1} lg={3} style={{display: "flex", justifyContent: "left", marginTop: "30px", width: "100%", maxWidth: "1000px"}}>
         {items.map(item => 
         <Col key={item.id} style={{display: "flex", justifyContent: "center", marginBottom: "30px"}}>
-          <StoreItem key={item.id} name={item.name} id={item.id} price={item.price}></StoreItem>
+          <StoreItem item={item}></StoreItem>
         </Col>
         )}
         </Row>
